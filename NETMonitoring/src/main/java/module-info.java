c14b8d29af0ed9320dfc86363e6cf79177d240f4
Hoa Ho <63163393+EpamelisAosinery@users.noreply.github.com>
2024-01-11 10:35:12 -0500

    devHoH - project setup2 (#2)
    * project setup
    
    * project setup

---
+++ NETMonitoring/src/main/java/module-info.java
@@ -0,0 +1,13 @@
+module edu.emu.netmonitoring {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens edu.emu.netmonitoring to javafx.fxml;
+    exports edu.emu.netmonitoring;
+}
