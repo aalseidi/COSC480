6ecbd94a878a36d925d90692cac1216e781eda7e
khammerm <khammermeister44@gmail.com>
2024-02-15 09:19:04 -0500

    Created line graph, just need to connect to dashboard w/ fxml

--- NETMonitoring/src/main/java/edu/emu/netmonitoring/NetMonitoringApplication.java
+++ NETMonitoring/src/main/java/edu/emu/netmonitoring/NetMonitoringApplication.java
@@ -7,13 +7,18 @@ import javafx.scene.chart.LineChart;
 import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.XYChart;
 import javafx.stage.Stage;
+
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 
 public class NetMonitoringApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(NetMonitoringApplication.class.getResource("/edu/emu/netmonitoring/fxml/dashboard.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 1200, 800);
+        //Scene scene = new Scene(fxmlLoader.load(), 1200, 800);
 
         stage.setTitle("Dashboard");
         final NumberAxis xAxis = new NumberAxis();
@@ -32,16 +37,36 @@ public class NetMonitoringApplication extends Application {
         series.getData().add(new XYChart.Data(3,7));
         series.getData().add(new XYChart.Data(4,21));
         series.getData().add(new XYChart.Data(5,23));
+        series.getData().add(new XYChart.Data(6,31));
+        series.getData().add(new XYChart.Data(7,17));
 
+        Scene scene  = new Scene(lineChart,800,600);
+        lineChart.getData().add(series);
 
         stage.setScene(scene);
         stage.show();
 
+    }
+    public static List<String> PingIpAddr(String ip) throws IOException
+    {
+        ProcessBuilder pb = new ProcessBuilder("ping", ip);
+        //ProcessBuilder pb = new ProcessBuilder("ping", "-c 5", ip);
+        BufferedReader stdInput = new BufferedReader(new InputStreamReader(pb.start().getInputStream()));        
 
+        while (!stdInput.ready())
+        {
+            // custom timeout handling
+        }
 
+        String line;
+        ArrayList<String> output = new ArrayList<>();
 
+        while ((line = stdInput.readLine()) != null)
+        {
+            output.add(line);
+        }
 
-
+        return output;
     }
 
     public static void main(String[] args) {
