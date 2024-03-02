e8ca66f8f7d08e9f52a7005e5623da73da4f3ab5
Jonathan <jtayl190@emich.edu>
2024-03-02 14:39:51 -0500

    Initial Commit.
    Does not display information in a nice way to the gui.
    But I believe it does connect to and list every network and it's status, as well as some additional information (whether we need that or not).

---
+++ NETMonitoring/src/main/java/edu/emu/netmonitoring/controller/NetworkLister.java
@@ -0,0 +1,31 @@
+package edu.emu.netmonitoring.controller;
+
+import java.net.NetworkInterface;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Enumeration;
+
+public class NetworkLister {
+
+    public static void listNetworkInterfaces() {
+        try {
+            Enumeration<NetworkInterface> interfaces = NetworkInterface.getNetworkInterfaces();
+            for (NetworkInterface iface : Collections.list(interfaces)) {
+                System.out.println("Interface Name: " + iface.getName());
+                System.out.println("Interface Display Name: " + iface.getDisplayName());
+                System.out.println("Is up? " + iface.isUp());
+                System.out.println("Is loopback? " + iface.isLoopback());
+                System.out.println("Is virtual? " + iface.isVirtual());
+                System.out.println("Hardware Address: " + Arrays.toString(iface.getHardwareAddress()));
+                System.out.println("MTU: " + iface.getMTU());
+                System.out.println("-------------------------------------------------------");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        listNetworkInterfaces();
+    }
+}
