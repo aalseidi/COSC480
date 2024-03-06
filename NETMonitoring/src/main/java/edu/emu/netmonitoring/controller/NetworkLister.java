afb4c07c411d7acfb68014cc977a8dadf21590c1
Jonathan <jtayl190@emich.edu>
2024-03-06 16:06:13 -0500

    Added Port Scanning so that Everything Could Be In One Place
    Issue #7

--- NETMonitoring/src/main/java/edu/emu/netmonitoring/controller/NetworkLister.java
+++ NETMonitoring/src/main/java/edu/emu/netmonitoring/controller/NetworkLister.java
@@ -1,6 +1,8 @@
 package edu.emu.netmonitoring.controller;
 
+import java.net.InetAddress;
 import java.net.NetworkInterface;
+import java.net.Socket;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.Enumeration;
@@ -18,11 +20,31 @@ public class NetworkLister {
                 System.out.println("Is virtual? " + iface.isVirtual());
                 System.out.println("Hardware Address: " + Arrays.toString(iface.getHardwareAddress()));
                 System.out.println("MTU: " + iface.getMTU());
+
+                Enumeration<InetAddress> inetAddresses = iface.getInetAddresses();
+                while (inetAddresses.hasMoreElements()) {
+                    InetAddress addr = inetAddresses.nextElement();
+                    System.out.println("IP Address: " + addr.getHostAddress());
+                    scanHost(String.valueOf(addr));
+                }
+
                 System.out.println("-------------------------------------------------------");
 
             }
         } catch (Exception e) {
             e.printStackTrace();
+        }
+    }
+
+    private static void scanHost(String hostAddress) {
+        for (int port = 1; port <= 200; port++) {
+            try (Socket socket = new Socket()) {
+                socket.connect(new java.net.InetSocketAddress(hostAddress, port), 100);
+                System.out.println("Port " + port + " is open on " + hostAddress);
+            } catch (Exception ignored) {
+                // Ignoring exceptions to keep the output clean. Closed ports will not be printed.
+                System.out.println("Port " + port + " is closed on " + hostAddress);
+            }
         }
     }
 
